420b1eea48531f45af0499edfd59cb6fa24a9ca1
Leonardo Faria <leonardofaria@gmail.com>
2015-06-21 14:29:58 -0300

    Atualizar path do ambiente

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'] . '/sisate';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
