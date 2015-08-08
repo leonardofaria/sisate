6c8eb5520df1b9abf0410814ec7f15d1854ddb7c
Leonardo Faria <leonardofaria@gmail.com>
2015-08-08 15:44:58 -0300

    Incluir manuais do sistema

--- application/views/ajuda/index.php
+++ application/views/ajuda/index.php
@@ -1,12 +1,11 @@
 <div class="panel panel-default">
 	<div class="panel-heading">
 		<h4 class="panel-title">
-			Ajuda
+			Manuais do sistema
 		</h4>
 	</div>
 	<div class="panel-body">
-
-		Em construção
-
+		<a href="<?php echo base_url('pub/manual.pdf'); ?>">Versão em PDF</a><br/>
+		<a href="<?php echo base_url('pub/manual.pptx'); ?>">Versão em PPTX (Power Point)</a>
 	</div>
 </div>
